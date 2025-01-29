4c796cbee687b57e7320ba3c2183395b362d5dc4
JJaje31 <161271739+JJaje31@users.noreply.github.com>
2025-01-29 16:24:40 -0700

    vercel deployment 4

--- Quiz/client/vite.config.js
+++ Quiz/client/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Ensure this matches your output directory
-  },
-
 })
